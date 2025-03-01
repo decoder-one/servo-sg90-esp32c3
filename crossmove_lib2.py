895e8a382f6812d8f183fb24d49a17aaff949528
futurecube_git <de-coder@mail.de>
2025-03-01 19:20:45 +0100

    Add files via upload

---
+++ crossmove_lib2.py
@@ -0,0 +1,21 @@
+#
+# example ESP32 servo
+# phil van allen
+#
+import time
+import machine
+from servo_2 import Servo
+
+servo_pin = machine.Pin(2)
+my_servo = Servo(servo_pin)
+delay = 0.01
+min = 15
+max = 165
+
+while True:
+    for i in range(min,max):
+      my_servo.write_angle(i)
+      time.sleep(delay)
+    for i in range(max, min, -1):
+      my_servo.write_angle(i)
+      time.sleep(delay)
